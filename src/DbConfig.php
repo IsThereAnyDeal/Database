1fbc1852f5e236008c6e3a6456eee5e5d0a42045
Tomáš Fedor <tomas.fedor3@gmail.com>
2022-01-27 23:01:41 +0100

    Fix config data

--- src/DbConfig.php
+++ src/DbConfig.php
@@ -7,16 +7,7 @@ use Nette\Schema\Schema;
 
 final class DbConfig
 {
-    private string $driver;
-    private string $host;
-    private int $port;
-    private string $password;
-    private string $database;
-    private string $defaultUser;
-    private bool $useCustomUsers;
-    private bool $useUserGroups;
-    private string $userPrefix;
-    private bool $profile;
+    private object $config;
 
     private function getSchema(): Schema {
         return Expect::structure([
@@ -34,56 +25,47 @@ final class DbConfig
     }
 
     public function __construct(array $config) {
-        $data = (new Processor())->process($this->getSchema(), $config);
-        $this->driver = $data['driver'];
-        $this->host = $data['host'];
-        $this->port = $data['port'];
-        $this->password = base64_decode($data['password']);
-        $this->database = $data['database'];
-        $this->defaultUser = $data['user'];
-        $this->useCustomUsers = $data['user_custom'];
-        $this->useUserGroups = $data['user_group'];
-        $this->userPrefix = $data['user_prefix'];
-        $this->profile = $data['profiler'];
+        $this->config = (new Processor())
+            ->process($this->getSchema(), $config);
     }
 
     public function getDriver(): string {
-        return $this->driver;
+        return $this->config->driver;
     }
 
     public function getHost(): string {
-        return $this->host;
+        return $this->config->host;
     }
 
     public function getPort(): int {
-        return $this->port;
+        return $this->config->port;
     }
 
     public function getPassword(): string {
-        return $this->password;
+        return $this->config->password;
     }
 
     public function getDatabase(): string {
-        return $this->database;
+        return $this->config->database;
     }
 
     public function getDefaultUser(): string {
-        return $this->defaultUser;
+        return $this->config->user;
     }
 
     public function useCustomUsers(): bool {
-        return $this->useCustomUsers;
+        return $this->config->user_custom;
     }
 
     public function useUserGroups(): bool {
-        return $this->useUserGroups;
+        return $this->config->user_group;
     }
 
     public function getUserPrefix(): string {
-        return $this->userPrefix;
+        return $this->config->user_prefix;
     }
 
     public function isProfile(): bool {
-        return $this->profile;
+        return $this->config->profiler;
     }
 }
