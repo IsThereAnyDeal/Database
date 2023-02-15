00f1e00b8b74efe71ad585f3b7bca8d176bce264
Tomáš Fedor <tomas.fedor3@gmail.com>
2023-02-15 16:53:09 +0100

    SubQuery doesn't have to be readonly

--- src/Sql/SubQuery.php
+++ src/Sql/SubQuery.php
@@ -10,6 +10,6 @@ class SubQuery
      * @param array<string, null|scalar|BackedEnum|list<null|scalar|BackedEnum>> $params
      */
     public function __construct(
-        public readonly string $query="",
-        public readonly array $params=[]) {}
+        public string $query="",
+        public array $params=[]) {}
 }
