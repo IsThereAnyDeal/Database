1d986b941aa36d4a652a5e543aa68000ee0fb0a7
Tomáš Fedor <tomas.fedor3@gmail.com>
2023-04-21 02:03:49 +0200

    Add SqlRawQuery

---
+++ src/Sql/Raw/SqlRawQuery.php
@@ -0,0 +1,39 @@
+<?php
+namespace IsThereAnyDeal\Database\Sql\Raw;
+
+use BackedEnum;
+use IsThereAnyDeal\Database\DbDriver;
+use IsThereAnyDeal\Database\Exceptions\InvalidParamTypeException;
+use IsThereAnyDeal\Database\Exceptions\MissingParameterException;
+use IsThereAnyDeal\Database\Exceptions\SqlException;
+use IsThereAnyDeal\Database\Sql\ParamParser;
+use IsThereAnyDeal\Database\Sql\SqlQuery;
+
+class SqlRawQuery extends SqlQuery {
+
+    private string $userQuery;
+
+    public function __construct(DbDriver $db, string $query) {
+        parent::__construct($db);
+        $this->userQuery = $query;
+    }
+
+    /**
+     * @param array<string, null|scalar|BackedEnum|list<null|scalar|BackedEnum>> $params
+     * @return int
+     * @throws InvalidParamTypeException
+     * @throws MissingParameterException
+     * @throws SqlException
+     */
+    final public function run(array $params=[]): int {
+        $parser = new ParamParser($this->userQuery, $params);
+        $values = $parser->getValues();
+        $query = $parser->getQuery();
+
+        $statement = $this->prepare($query, $values);
+        $this->execute($statement);
+
+        return $statement->rowCount();
+    }
+
+}
