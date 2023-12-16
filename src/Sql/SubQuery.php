d5f5ad3c0e3e5c8a612ad79ea6952ef40478b0a3
Tomáš Fedor <tomas.fedor3@gmail.com>
2023-12-16 01:57:11 +0100

    Add hints to SubQuery

--- src/Sql/SubQuery.php
+++ src/Sql/SubQuery.php
@@ -8,8 +8,11 @@ class SubQuery
     /**
      * @param string $query
      * @param array<string, null|scalar|BackedEnum|list<null|scalar|BackedEnum>> $params
+     * @param list<string> $hints
      */
     public function __construct(
         public string $query="",
-        public array $params=[]) {}
+        public array $params=[],
+        public readonly array $hints=[]
+    ) {}
 }
